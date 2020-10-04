676fd55f20fbcc8546e539281c22359f55b9fdf9
chong <chong@smartshell.cn>
2020-10-04 13:53:29 -0700

    hide or show footer

--- src/router/index.js
+++ src/router/index.js
@@ -14,19 +14,31 @@ export default new Router({
   routes: [
     {
       path: '/msite',
-      component: MSite
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/order',
-      component: Order
+      component: Order,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/',
